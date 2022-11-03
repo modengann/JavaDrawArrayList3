e8e7ac3fb21e0902b76d0f20c410b77f0cb86813
modengann <99417989+modengann@users.noreply.github.com>
2022-11-03 11:25:16 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,31 @@
+/** YouTube: https://youtu.be/WoNhsqTVBsg
+ *  Goal: Have rain randomly appear at the top of the screen and fall down to the bottom of the screen
+ */
+
+import javadraw.*;
+import java.util.*;
+
+public class App extends Window{
+    ArrayList<Oval> rain;
+    
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    //Task 1: Copy this method for part 2
+    //Task 4: Use addRain() to create rain on the screen
+    public void start(){
+
+    }
+
+    //Task 2: Copy this method from part 2
+    public void rainFall(){
+
+    }
+
+    //Task 3: Create a method that will cause rain to randomly appear across the top of the screen
+    public void addRain(){
+
+    }
+
+}
